6f9059e0ba5924b9e20b5811a6361fd36874ba13
André <AndreR@ppedv.de>
2023-08-16 16:09:02 +0200

    DbTest für CarRentalXPress

---
+++ ppedv.CarRentalXPress/ppedv.CarRentalXPress.Data.EfCore.Tests/CarRentalXPressContextTests.cs
@@ -0,0 +1,67 @@
+using AutoFixture;
+using AutoFixture.Kernel;
+using FluentAssertions;
+using ppedv.CarRentalXPress.Model;
+using System.Reflection;
+
+namespace ppedv.CarRentalXPress.Data.EfCore.Tests
+{
+    public class CarRentalXPressContextTests
+    {
+        string conString = "Server=(localdb)\\mssqllocaldb;Database=CarRentalXPress_Tests;Trusted_Connection=true";
+
+        [Fact]
+        public void Can_create_Db()
+        {
+            var con = new CarRentalXPressContext(conString);
+            con.Database.EnsureDeleted();
+
+            var result = con.Database.EnsureCreated();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Can_create_and_read_Car_with_AutoFixture()
+        {
+            var fix = new Fixture();
+            fix.Customizations.Add(new PropertyNameOmitter("Id"));
+            fix.Behaviors.Add(new OmitOnRecursionBehavior());
+            var car = fix.Create<Car>();
+
+            using (var con = new CarRentalXPressContext(conString))
+            {
+                con.Database.EnsureCreated();
+                con.Add(car);
+                con.SaveChanges();
+            }
+            using (var con = new CarRentalXPressContext(conString))
+            {
+                var loaded = con.Cars.Find(car.Id);
+
+                loaded.Should().BeEquivalentTo(car, x => x.IgnoringCyclicReferences());
+            }
+
+        }
+
+    }
+
+    internal class PropertyNameOmitter : ISpecimenBuilder
+    {
+        private readonly IEnumerable<string> names;
+
+        internal PropertyNameOmitter(params string[] names)
+        {
+            this.names = names;
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            var propInfo = request as PropertyInfo;
+            if (propInfo != null && names.Contains(propInfo.Name))
+                return new OmitSpecimen();
+
+            return new NoSpecimen();
+        }
+    }
+}
---
+++ ppedv.CarRentalXPress/ppedv.CarRentalXPress.Data.EfCore/CarRentalXPressContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.CarRentalXPress.Model;
+
+namespace ppedv.CarRentalXPress.Data.EfCore
+{
+    public class CarRentalXPressContext : DbContext
+    {
+        private readonly string conString;
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Rent> Rents { get; set; }
+
+        public CarRentalXPressContext(string conString)
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(conString).UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Car>().Property(x => x.KW).HasColumnName("Leistungggggg");
+        }
+
+    }
+}

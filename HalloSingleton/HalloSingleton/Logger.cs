3769e74543b5927184d0319a6b280e48a70e449a
André <AndreR@ppedv.de>
2023-08-16 10:01:57 +0200

    Singleton

---
+++ HalloSingleton/HalloSingleton/Logger.cs
@@ -0,0 +1,38 @@
+﻿namespace HalloSingleton
+{
+    internal class Logger
+    {
+        private static Logger _instance;
+        private static object _syncLock = new object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    lock (_syncLock)
+                    {
+                        if (_instance == null)
+                            _instance = new Logger();
+                    }
+
+                return _instance;
+            }
+        }
+
+        private Logger()
+        {
+            Info("Neuer Logger");
+        }
+
+        public void Info(string msg)
+        {
+            Console.WriteLine($"[INFO] {DateTime.Now:g} {msg}");
+        }
+
+        public void Error(string msg)
+        {
+            Console.WriteLine($"[ERROR] {DateTime.Now:g} {msg}");
+        }
+    }
+}
---
+++ HalloSingleton/HalloSingleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using HalloSingleton;
+
+Console.WriteLine("Hello, World!");
+
+
+
+for (int i = 0; i < 10; i++)
+{
+    Task.Run(() => Logger.Instance.Info("Moin"));
+ 
+}
+
+Logger.Instance.Error("PANiKK!!!11");

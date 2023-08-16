8e7e57e3c369012a07e5c22e8d5a2a7d9072cb60
André <AndreR@ppedv.de>
2023-08-16 11:35:45 +0200

    Deco

---
+++ Decorator/Decorator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Decorator;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Hello, World!");
+
+var p1 = new Käse(new Salami(new Käse(new Pizza())));
+
+Console.WriteLine($"{p1.Name} {p1.Price:c}");

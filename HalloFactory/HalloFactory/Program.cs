a1de2374fd8ae315ee6585f0dcc6a19ab4e34032
André <AndreR@ppedv.de>
2023-08-16 10:27:36 +0200

    Factory

---
+++ HalloFactory/HalloFactory/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using System.Data.Common;
+
+Console.WriteLine("Hello, World!");
+
+var conString = "Server=(localdb)\\mssqllocaldb;Database=Northwnd;Trusted_Connection=true";
+conString = "Data Source=C:\\DB\\Northwind.sqlite;";
+//DbProviderFactory factory = SqlClientFactory.Instance;
+DbProviderFactory factory = SqliteFactory.Instance;
+
+DbConnection con = factory.CreateConnection();
+con.ConnectionString = conString;
+con.Open();
+
+DbCommand cmd = factory.CreateCommand();
+cmd.Connection = con;
+cmd.CommandText = "SELECT * FROM Employees";
+
+DbDataReader reader = cmd.ExecuteReader();
+
+while (reader.Read())
+{
+    Console.WriteLine($"{reader.GetString(reader.GetOrdinal("LastName"))} ");
+}

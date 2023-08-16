00e1b503e934ae40351c06ae8d6636f8586ca2a0
André <AndreR@ppedv.de>
2023-08-16 14:49:29 +0200

    Template für WinForms

---
+++ WinFormsCustomControl/WinFormsCustomControl/Form1.cs
@@ -0,0 +1,10 @@
+namespace WinFormsCustomControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WinFormsCustomControl/WinFormsCustomControl/MyButton.cs
@@ -0,0 +1,16 @@
+﻿namespace WinFormsCustomControl
+{
+    internal class MyButton : Button
+    {
+        int paintCount = 0;
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            
+
+            pevent.Graphics.FillRectangle(Brushes.Blue, ClientRectangle);
+            pevent.Graphics.DrawString($"Paint: {paintCount++}",SystemFonts.DefaultFont,Brushes.BlanchedAlmond,ClientRectangle);
+
+        }
+        
+    }
+}

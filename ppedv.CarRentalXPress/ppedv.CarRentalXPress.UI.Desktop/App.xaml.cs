cc803f1a5c619cbd0c7fe9ce7e560bf06c104d71
André <AndreR@ppedv.de>
2023-08-17 12:45:22 +0200

    WPF UI

---
+++ ppedv.CarRentalXPress/ppedv.CarRentalXPress.UI.Desktop/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ppedv.CarRentalXPress.UI.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

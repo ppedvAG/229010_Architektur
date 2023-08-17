8595af0bd2a1fc9d6ee7ef7e8728c99836a4eeee
André <AndreR@ppedv.de>
2023-08-17 16:06:13 +0200

    ViewModel mit Community Toolkit

--- ppedv.CarRentalXPress/ppedv.CarRentalXPress.UI.Desktop/ViewModels/CarsViewModel.cs
+++ ppedv.CarRentalXPress/ppedv.CarRentalXPress.UI.Desktop/ViewModels/CarsViewModel.cs
@@ -1,32 +1,55 @@
-﻿using ppedv.CarRentalXPress.Model;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ppedv.CarRentalXPress.Model;
 using ppedv.CarRentalXPress.Model.Contracts;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
 
 namespace ppedv.CarRentalXPress.UI.Desktop.ViewModels
 {
-    public class CarsViewModel : INotifyPropertyChanged
+    public class CarsViewModel : ObservableObject
     {
         private readonly IRepository repo;
         private Car selectedCar;
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-
         public CarsViewModel(IRepository repo)
         {
             this.repo = repo;
-            CarList = new List<Car>(repo.GetAll<Car>());
+            CarList = new ObservableCollection<Car>(repo.GetAll<Car>());
 
-            SaveCommand = new SaveCommand(repo);
+            //SaveCommand = new SaveCommand(repo);
+            SaveCommand = new RelayCommand(() => repo.SaveAll());
+            NewCommand = new RelayCommand(UserWantsToAddNewCar);
+            DeleteCommand = new RelayCommand(UserWantsToDeleteSelectedCar);
+        }
+
+        private void UserWantsToDeleteSelectedCar()
+        {
+            if(selectedCar!=null)
+            {
+                repo.Delete(selectedCar);
+                CarList.Remove(selectedCar);
+            }
+        }
+
+        private void UserWantsToAddNewCar()
+        {
+            var car = new Car() { Color = "NEU", Manufacturer = "NEU", Model = "NEU", KW = 12 };
+            repo.Add(car);
+            CarList.Add(car);
+            SelectedCar = car;
         }
 
         //todo KILL IT
-        public CarsViewModel() : this(new Data.EfCore.CarRentalXPressContextRepositoryAdapter ("Server=(localdb)\\mssqllocaldb;Database=CarRentalXPress_Tests;Trusted_Connection=true"))
+        public CarsViewModel() : this(new Data.EfCore.CarRentalXPressContextRepositoryAdapter("Server=(localdb)\\mssqllocaldb;Database=CarRentalXPress_Tests;Trusted_Connection=true"))
         { }
 
-        public List<Car> CarList { get; set; }
+        public ObservableCollection<Car> CarList { get; set; }
 
         public ICommand SaveCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand NewCommand { get; set; }
 
         public Car SelectedCar
         {
@@ -36,7 +59,10 @@ namespace ppedv.CarRentalXPress.UI.Desktop.ViewModels
                 selectedCar = value;
                 //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PS)));
                 //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedCar)));
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(""));
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(""));
+                OnPropertyChanged(nameof(PS));
+                OnPropertyChanged(nameof(KW));
+                OnPropertyChanged(nameof(SelectedCar));
 
             }
         }
@@ -48,10 +74,12 @@ namespace ppedv.CarRentalXPress.UI.Desktop.ViewModels
             {
                 if (selectedCar != null)
                     selectedCar.KW = value;
-                
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("KW"));
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PS"));
-           }
+
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("KW"));
+                OnPropertyChanged("KW");
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PS"));
+                OnPropertyChanged("PS");
+            }
         }
 
         public string PS

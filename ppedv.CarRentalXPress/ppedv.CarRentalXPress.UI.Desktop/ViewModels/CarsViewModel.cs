015ffe128dbc2519655b4b6683dfa779711a9d2a
André <AndreR@ppedv.de>
2023-08-17 15:13:05 +0200

    Car View und Model zeug mit KW in PS

--- ppedv.CarRentalXPress/ppedv.CarRentalXPress.UI.Desktop/ViewModels/CarsViewModel.cs
+++ ppedv.CarRentalXPress/ppedv.CarRentalXPress.UI.Desktop/ViewModels/CarsViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using ppedv.CarRentalXPress.Model;
 using ppedv.CarRentalXPress.Model.Contracts;
+using System.ComponentModel;
 
 namespace ppedv.CarRentalXPress.UI.Desktop.ViewModels
 {
-    public class CarsViewModel
+    public class CarsViewModel : INotifyPropertyChanged
     {
         private readonly IRepository repo;
+        private Car selectedCar;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         public CarsViewModel(IRepository repo)
         {
@@ -19,6 +23,41 @@ namespace ppedv.CarRentalXPress.UI.Desktop.ViewModels
 
         public List<Car> CarList { get; set; }
 
-        public Car SelectedCar { get; set; }
+        public Car SelectedCar
+        {
+            get => selectedCar;
+            set
+            {
+                selectedCar = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PS)));
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedCar)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(""));
+
+            }
+        }
+
+        public int KW
+        {
+            get => selectedCar == null ? -1 : selectedCar.KW;
+            set
+            {
+                if (selectedCar != null)
+                    selectedCar.KW = value;
+                
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("KW"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PS"));
+           }
+        }
+
+        public string PS
+        {
+            get
+            {
+                if (SelectedCar == null)
+                    return "---";
+
+                return (SelectedCar.KW * 1.36).ToString("0.00");
+            }
+        }
     }
 }
